aa4c26f4cb27e47e3cfb69211ecb6af2d37b5dcb
Shayan Halder <shayanhalder2389@gmail.com>
2026-02-06 12:11:28 -0800

    add results.py for deliverables

---
+++ results.py
@@ -0,0 +1,23 @@
+import shelve
+
+SHELVE_FILE = "frontier.shelve"
+save = shelve.open(SHELVE_FILE)
+def num_unique_pages():
+    print("Number of unique pages: ", len(save.keys()) - 3)
+
+def longest_page():
+    print("Longest page: ", save['longest_page'])
+
+def most_common_words(k=50):
+    print(f"{k} most common words: ")
+    n = 0
+    for k, v in save.items():
+        if n == k: break
+        print(f"{k}: {v}  ", end='')
+        n += 1
+
+def subdmoains():
+    print(f"{len(save['subdomain_frequencies'].keys())} subdomains found: ")
+    print(save['subdomain_frequencies'])
+
+
